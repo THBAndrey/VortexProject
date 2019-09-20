d858c443d8007edeea06a5fdfe015ed3bace33ed
Andrey <lazarev.and_18@list.ru>
2019-09-20 23:39:40 +0300

    Base setup core api

--- VortexApi/VortexCore/WebApplication1/Program.cs
+++ VortexApi/VortexCore/WebApplication1/Program.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using VortexCore;
 
-namespace WebApplication1
+namespace VortexCore
 {
     public class Program
     {
