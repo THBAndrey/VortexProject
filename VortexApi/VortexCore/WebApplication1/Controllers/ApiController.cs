0bc93a9725f3e9c40fd9a1cbbf14fec5db28542b
Andrey <lazarev.and_18@list.ru>
2019-10-03 02:55:39 +0300

    VortexCore: Add custom AuthHandler

--- VortexApi/VortexCore/WebApplication1/Controllers/ApiController.cs
+++ VortexApi/VortexCore/WebApplication1/Controllers/ApiController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace VortexCore.Controllers
 {
+    
     [Route("[controller]/[action]")]
     [ApiController]
     public class ApiController : ControllerBase
@@ -21,7 +22,7 @@ namespace VortexCore.Controllers
         {
             ManagerDB = new VortexManager(context);
         }
-        //get: api/SendMessage
+
         public ActionResult SendMessage()
         {
             FirebaseControl.SendMessage().ContinueWith((task) =>
@@ -56,12 +57,10 @@ namespace VortexCore.Controllers
             }
         }
 
+        [Authorize]
         public async Task<ActionResult> SecureResource()
         {
-            //var jwtToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjI5NGNlMzNhNWQ3MmI0NjYyNzI3ZGFiYmRhNzVjZjg4Y2Y5OTg4MGUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdm9ydGV4LTRiNmRiIiwiYXVkIjoidm9ydGV4LTRiNmRiIiwiYXV0aF90aW1lIjoxNTY5ODcyNjQxLCJ1c2VyX2lkIjoiRnBvQmdST0VwMlZmQzZmUmtJRHI0SjZUVzNtMiIsInN1YiI6IkZwb0JnUk9FcDJWZkM2ZlJrSURyNEo2VFczbTIiLCJpYXQiOjE1Njk4NzM3NTEsImV4cCI6MTU2OTg3NzM1MSwiZW1haWwiOiJsYXphcmV2YW5kLjIwQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJsYXphcmV2YW5kLjIwQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.KcwWFBP7JHWEtyxdIYO-jWGbWbqn2a92upL2M6UeNDUjgQivM9ppd-cKp82k76yIAshXIh6ihgIjYIG4Bg21c6BqOCkj9LD3Mr9diU3dW8pb_mpaOLjIwRIlSIR8dIZldBerY3ZpgiAxHXfW2lnl_MKa3J9kepHYJQrGBK-stBNyPfm2-wl8bUCUs8YgM6ThCViSPZUadtLn-FvmjFscvwIJ1LDz7kvMSTNu52N5FUOE9K5KJKWgrZfrsBF3zASp0zpnoNSLf5I6AEAHUW1rWbDIbGZrMfWoX2i218WcNCr7LzyAtdIAipLH9W6lRktVo1BnonUP0uYj_W8u4CcpEg";
-            //var fireBaseUser = await FirebaseAdmin.Auth.FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(jwtToken);
-            //var uuser = await FirebaseAdmin.Auth.FirebaseAuth.DefaultInstance.GetUserAsync(fireBaseUser.Uid);
-            //var user = User.Claims;
+            var user = User.Claims;
             return new JsonResult("SecureResource");
         }
     }
