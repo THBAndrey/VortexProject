86f7384b03da3518076a7de78db65539406dfea9
Andrey <lazarev.and_18@list.ru>
2019-10-18 02:17:10 +0300

    MongoDB and Chat support
    VortexCore: add MongoDB support, add signalr and base chathub
    VortexWeb: add signalr and base chat support

---
+++ VortexApi/VortexCore/WebApplication1/ModelsDB/MongoDB/ChatDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VortexCore.ModelsDB.MongoDB
+{
+    public interface IChatDatabaseSettings
+    {
+        string ChatCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+
+    public class ChatDatabaseSettings : IChatDatabaseSettings
+    {
+        string IChatDatabaseSettings.ChatCollectionName { get; set; }
+        string IChatDatabaseSettings.ConnectionString { get; set; }
+        string IChatDatabaseSettings.DatabaseName { get; set; }
+    }
+}
--- VortexWeb/pages/cabinet/chat.vue
+++ VortexWeb/pages/cabinet/chat.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="h-full flex items-stretch">
         <div class="flex-1 h-full flex flex-col items-stretch pr-3">
-            <div class="flex-1 overflow-auto flex flex-col pr-2 mb-3">
-                <div v-ripple.dblclick class="message-item bg-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, dolores.</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">Ea fugiat obcaecati atque quae. Ipsum magnam repudiandae nobis necessitatibus unde illo vero, cupiditate vel adipisci maiores quas autem perspiciatis ipsa eius?</div>
-                <div v-ripple.dblclick class="message-item mine bg-gray-200">Vero, earum labore, repellendus consequatur ipsa incidunt nesciunt dolorum hic vel eius soluta itaque similique id! Expedita pariatur dolore repellat quos fugit!</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">Aliquid animi voluptatem quo cumque debitis assumenda delectus quod magni illo? Corrupti sed quis, excepturi autem expedita consectetur perferendis voluptas qui molestiae!</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">Exercitationem assumenda quasi nemo hic quibusdam. Dolorum ad at officiis nulla iste necessitatibus sed, aperiam maiores quae distinctio sint libero debitis unde.</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">Totam, pariatur vel inventore temporibus ea natus est nostrum! Quaerat sit commodi repellat itaque aliquam nesciunt corporis quos tempora, accusantium, sapiente porro?</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">A, harum distinctio saepe placeat odit quam earum consectetur esse maxime assumenda ea possimus sint natus dolor culpa modi illum quae perspiciatis.</div>
-                <div v-ripple.dblclick class="message-item mine bg-gray-200">Facere laboriosam iste quisquam nisi excepturi assumenda. Obcaecati quae explicabo, quibusdam placeat architecto saepe facilis repellendus? Distinctio reiciendis eum autem illo ad?</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">Molestiae voluptas minima ab quam enim architecto tempore itaque soluta perspiciatis, cumque ex corporis quos facere, ducimus, voluptates sequi esse quasi fuga.</div>
-                <div v-ripple.dblclick class="message-item bg-gray-200">Eos maiores, odio recusandae voluptate aliquid nam provident mollitia amet ipsa nesciunt fuga minima suscipit itaque vero dicta voluptates iusto vitae. Quam?</div>
+            <div ref="messagesArea" class="flex-1 overflow-auto flex flex-col pr-2 mb-3">
+                <div v-for="(message, index) in messages" :key="index" v-ripple.dblclick :class="message.userId == 0 ? 'mine' : ''" class="message-item bg-gray-200">
+                    <span style="white-space: pre-line">{{ message.messageText }}</span>
+                </div>
             </div>
-            <div class="bg-gray-200" style="height: 100px;">
-
+            <div class="bg-gray-200 flex items-stretch" style="height: 100px;">
+                <div class="flex-1 pl-3 pt-3 pb-3">
+                    <textarea v-model="messageText" @keydown.ctrl.enter="sendMessage" class="w-full h-full" style="resize: none;"></textarea>
+                </div>
+                <div>
+                    <button @click="sendMessage" v-ripple class="h-full" style="width: 70px;">
+                        <sui-icon size="large" name="paper plane"/>
+                    </button>
+                </div>
             </div>
         </div>
         <div class="bg-gray-300" style="width: 300px;">
@@ -24,10 +24,51 @@
 </template>
 
 <script>
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
+
 export default {
     middleware: 'auth',
     layout: 'cabinet',
+    data(){
+        return{
+            connection: null,
+            messages: [
+                { messageText: 'Welcome to chat', userId: 0 }
+            ],
+            messageText: ''
+        }
+    },
+    methods: {
+        initChatConnection(){
+            this.connection = new HubConnectionBuilder()
+                .withUrl('https://localhost:4000/chat')
+                .configureLogging(LogLevel.Information)
+                .build()
 
+            this.connection.on("ReceiveMessage", (user, message) => {
+                this.messages.unshift({ messageText: message, userId: user })
+            })
+
+            this.connection.start()
+        },
+        sendMessage(){
+            function isNullOrWhitespace(input) {
+                if (typeof input === 'undefined' || input == null) return true
+                return input.replace(/\s/g, '').length < 1
+            }
+
+            if(!isNullOrWhitespace(this.messageText)){
+                this.connection.invoke("SendMessage", 'User', this.messageText).then(() => {
+                    this.messageText = ''
+                })
+            }
+        }
+    },
+    created(){
+        if(process.browser){
+            this.initChatConnection()
+        }
+    }
 }
 </script>
 
